a4f9fa366f467ab24f9d3a59b142104a0faedc29
AnushaMahesh18 <am4556@g.rit.edu>
2026-02-11 15:03:13 -0500

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
-yum update -y
-dnf install php8.1 -y
-dnf install php8.1-mysqlnd.x86_64 -y
-service httpd restart
-sudo yum install -y mariadb105-server
-service mariadb start
-mysqladmin -uroot create blog
+set -e
 
-systemctl start httpd
-systemctl enable httpd
+# Update packages
+dnf update -y
 
-systemctl start mariadb
-systemctl enable mariadb
+# Install Apache, PHP, and MariaDB (local DB)
+dnf install -y httpd wget tar mariadb105-server php php-mysqlnd
 
-mysql -e "CREATE DATABASE blog;"
-mysql -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'password';"
-mysql -e "GRANT ALL PRIVILEGES ON blog.* TO 'root'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
+# Start + enable services
+systemctl enable --now httpd
+systemctl enable --now mariadb
 
+# Create WordPress DB + user (local)
+DB_NAME="blog"
+DB_USER="wpuser"
+DB_PASS="password"
+
+mysql -u root <<SQL
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
+FLUSH PRIVILEGES;
+SQL
+
+# Download and set up WordPress under /var/www/html/blog
 cd /var/www/html
-wget https://wordpress.org/latest.tar.gz
+wget -q https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
+rm -f latest.tar.gz
+
+# Put wordpress files into /var/www/html/blog
+rm -rf blog
 mv wordpress blog
-cd blog
+
+# Configure wp-config.php
+cd /var/www/html/blog
 cp wp-config-sample.php wp-config.php
 
-sed -i 's/database_name_here/blog/g' wp-config.php
-sed -i 's/username_here/root/g' wp-config.php
-sed -i 's/password_here/password/g' wp-config.php               
+sed -i "s/database_name_here/${DB_NAME}/" wp-config.php
+sed -i "s/username_here/${DB_USER}/" wp-config.php
+sed -i "s/password_here/${DB_PASS}/" wp-config.php
 
-chown -R apache:apache /var/www/html/
+# Permissions
+chown -R apache:apache /var/www/html
 
-mysql_secure_installation <<EOF
-
-
-      y
-      password
-      password
-      y
-      y
-      y
-      y
+# Restart apache
+systemctl restart httpd
