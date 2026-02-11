923c8cfe469746aaa5943e98b79076b0c920a848
AnushaMahesh18 <am4556@g.rit.edu>
2026-02-11 14:29:49 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,7 @@
-# Output to display the private IP address of the EC2 instance
 output "instance_ip_addr" {
-  value       = aws_instance.my_server.private_ip
-  description = "The private IP address of the EC2 instance"
+  value = aws_instance.wordpress_ec2.private_ip
 }
 
-# REQUIREMENT #2: Output the public IP address
 output "public_ip" {
-  value       = aws_instance.my_server.public_ip
-  description = "The public IP address of the EC2 instance - use this to access WordPress"
+  value = aws_instance.wordpress_ec2.public_ip
 }
